0fee7747550e7de2c20572c1ffe93939b8396c85
Burak Kartal <futuregnr@gmail.com>
2026-01-07 19:47:50 +0300

    AppServiceProvider moved to CoreServiceProvider

--- src/Providers/CoreServiceProvider.php
+++ src/Providers/CoreServiceProvider.php
@@ -2,18 +2,19 @@
 
 namespace LaraModule\Core\Providers;
 
-use LaraModularity\Providers\ModuleServiceProvider;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\ServiceProvider;
+use LaraModule\Core\Middleware\ApiLocale;
 
-class CoreServiceProvider extends ModuleServiceProvider
+class CoreServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        parent::register();
-
-        // Add module-specific registration logic here if needed
+        //
     }
 
     /**
@@ -21,8 +22,22 @@ class CoreServiceProvider extends ModuleServiceProvider
      */
     public function boot(): void
     {
-        parent::boot();
+        if (config('app.debug')) {
+            // Macro for query debugging purposes.
+            QueryBuilder::macro('toSqlWithBindings', function (): string {
+                /** @var \Illuminate\Database\Query\Builder $this */
+                $sql = $this->toSql();
+                $bindings = $this->getBindings();
 
-        // Add module-specific boot logic here if needed
+                return vsprintf(str_replace('?', "'%s'", $sql), array_map('addslashes', $bindings));
+            });
+
+            EloquentBuilder::macro('toSqlWithBindings', function (): string {
+                /** @var \Illuminate\Database\Eloquent\Builder $this */
+                return $this->getQuery()->toSqlWithBindings();
+            });
+        }
+
+        $this->app['router']->pushMiddlewareToGroup('api', ApiLocale::class);
     }
 }
